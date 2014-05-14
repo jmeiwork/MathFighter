ddcfde44a812cf893e2a83f60ffdf429f1dd3b91
jmeiwork <justinmei1@gmail.com>
2014-05-14 15:28:01 -0500

    dfsd

---
+++ MathFighter/ChatConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Infrastructure;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace MathFighter
+{
+    [HubName("chatHub")]
+    public class ChatHub : Hub
+    {
+        public void JoinRoom(string room)
+        {
+            Groups.Add(Context.ConnectionId, room);
+
+            Clients.Group(room).newJoinAnnouncement("client wit ID: " + Context.ConnectionId + " Joined");
+
+        }
+
+        public void SendMessageForRoom(string room, string message)
+        {
+            Clients.Group(room).newMessage(message);
+        }
+
+    }
+
+}
