d37e7cc2746ce256cf31a4264ee99e83bd37f926
jmeiwork <justinmei1@gmail.com>
2014-05-14 16:43:45 -0500

    adding basic framework

---
+++ MathManager/Equation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathManager
+{
+    public class Equation
+    {
+        public Equation()
+        {
+            Numbers = new List<decimal>();
+            Operators = new List<string>();
+        }
+        public List<decimal> Numbers { get; set; }
+        public List<string> Operators { get; set; }
+        public decimal Answer { get; set; }
+
+    }
+}
---
+++ MathManager/MathManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathManager
+{
+    public class MathManager
+    {
+        public Equation GetMathEquation()
+        {
+            var returnEquation = new Equation();
+
+            var random = new Random();
+
+            var number1 = random.Next(0, 30);
+            var number2 = random.Next(0, 30);
+
+            returnEquation.Numbers.Add(number1);
+            returnEquation.Numbers.Add(number2);
+
+            returnEquation.Operators.Add("+");
+
+            returnEquation.Answer = number1 + number2;
+
+            return returnEquation;
+        }
+    }
+}
